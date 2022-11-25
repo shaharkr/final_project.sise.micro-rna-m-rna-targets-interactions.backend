ee14ad9cc8944fda76d079a49272bfee66a0b013
Ben Aidlin <74815296+BenAidlin@users.noreply.github.com>
2022-11-25 15:14:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -14,6 +14,6 @@ def hello_world():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=8080)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
