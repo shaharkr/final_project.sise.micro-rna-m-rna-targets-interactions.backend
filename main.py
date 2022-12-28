6430c486e761e2c4164285bd35f73cd91ce25ad9
shaharkr <shahar.kramer@gmail.com>
2022-12-28 18:48:21 +0200

    add configuration object and finish endpoint /organisms/details without search options

--- main.py
+++ main.py
@@ -2,29 +2,45 @@ from flask import Flask, jsonify, send_from_directory, request
 from flask_cors import CORS, cross_origin
 import os
 from flask_sqlalchemy import SQLAlchemy
+from configurator import Configurator
+
 
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
-DB_URL = 'postgresql://postgres:postgrespass@132.73.84.177:8080/micro-message-rna-project'
+config = Configurator()
+        
+DB_URL = config.get_db_url()
 
 app.config['SQLALCHEMY_DATABASE_URI'] = DB_URL
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # silence the deprecation warning
 
 db = SQLAlchemy(app)
 
-class Organisim(db.Model):
+class Organism(db.Model):
     __tablename__ = 'organisms'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(200), nullable=False)
+    data_sets = db.relationship('DataSet', lazy=True,backref='organism')
 
     def __init__(self, id, name):
         self.id = id
         self.name = name
     
     def __repr__(self):
-        return f"<Organism {self.name}>"
+        return f"<Organism: id-{self.id}, name-{self.name}>"
+
+class DataSet(db.Model):
+    __tablename__ = 'data_set'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    data_set_mb = db.Column(db.Float)
+    interactions_amount = db.Column(db.Integer)
+    organism_id = db.Column(db.Integer, db.ForeignKey('organisms.id'))
+    
+    def __repr__(self):
+        return f"<DataSet: id-{self.id}, oranism-{self.organism}>"
 
 class Interaction(db.Model):
     __tablename__ = 'mirna_mrna_interactions'
@@ -51,29 +67,36 @@ def hello_world():
     return jsonify(to_ret)
 
 
-@app.route('/api/organisms', methods=['GET'])
-def handle_cars():
-    cars = Organisim.query.all()
-    results = [
-        {
-            "id": car.id,
-            "name": car.name
-        } for car in cars]
+@app.route('/api/organisms/details', methods=['GET'])
+def get_organisms_details():
+    data_sets_dict = get_data_sets()
+    get_search_options()
+    organisms_dict = {}
+    for data_set in data_sets_dict.values():
+        if data_set["organism"].id not in organisms_dict:
+            organisms_dict[data_set["organism"].id] = {"id": data_set["organism"].id,
+                                                    "name": data_set["organism"].name,
+                                                    "datasets": []}
+        organisms_dict[data_set["organism"].id]["datasets"].append(data_set)
+        data_set.pop("organism")
+    organisms_list = list(organisms_dict.values())
+    return organisms_list
 
-    return {"count": len(results), "orgs": results}
+def get_data_sets():
+    data_sets = db.session.query(DataSet)
+    data_sets_dict = {}
+    for data_set in data_sets:
+        data_sets_dict[data_set.id] = { "id": data_set.id,
+                                        "name": data_set.name,
+                                        "interactionsAmount": data_set.interactions_amount,
+                                        "datasetMB": data_set.data_set_mb,
+                                        "searchOptions": {"seedFamilies": [], "miRnaIds": [], "miRnaSeqs": [],
+                                                        "sites": [], "geneIds": [], "regions": []},
+                                        "organism" : data_set.organism}
+    return data_sets_dict
 
-@app.route('/api/interaction/<int:int_id>', methods=['GET'])
-def abc(int_id):
-    cars = Interaction.query.filter_by(data_set_id=int_id).all()
-    results = [
-        {
-            "data_set_id": car.data_set_id,
-            "index": car.index,
-            "sequence": car.sequence
-        } for car in cars]
-
-    return {"count": len(results), "inters": results}
-
+def get_search_options():
+    pass
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
