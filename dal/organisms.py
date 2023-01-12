f3b06b30492f5989af142791bb89848c666a3262
shaharkr <shahar.kramer@gmail.com>
2023-01-12 10:46:14 +0200

    create function that make list of interaction

--- dal/organisms.py
+++ dal/organisms.py
@@ -1,6 +1,7 @@
 from dal.db_connection import DataSet, db, executor
 from dal.db_connection import SeedFamilyOption, SiteOption, GeneIdOption, RegionOption, mirnaIdOption
 from dal.db_connection import Interaction
+from dal.interactions import create_interactions_list
 
 
 def get_organisms(with_options=False):
@@ -88,23 +89,7 @@ def get_data_set_interactions(data_set_id):
     interactions = []
     try:
         results = Interaction.query.filter_by(data_set_id=data_set_id).limit(750).all()
-        for interaction in results:
-            interactions.append({"index": interaction.index,
-                                "datasetId": interaction.data_set_id,
-                                "miRnaId": interaction.mirna_id,
-                                "miRnaSeq": interaction.mirna_sequence,
-                                "seedFamily": interaction.seed_family,
-                                "site": interaction.site,
-                                "region": interaction.region,
-                                "start": interaction.start,
-                                "end": interaction.end,
-                                "mrnaBulge": interaction.mrna_bulge,
-                                "mrnaInter": interaction.mrna_inter,
-                                "mirInter": interaction.mir_inter,
-                                "mirBulge": interaction.mir_bulge,
-                                "energyMefDuplex": interaction.Energy_MEF_Duplex,
-                                "geneId": interaction.Gene_ID
-                            })
+        interactions = create_interactions_list(results)
     except Exception as e:
         print(f'dal failed to get interactions of data set id- {data_set_id}. error: {str(e)}')
     return interactions
