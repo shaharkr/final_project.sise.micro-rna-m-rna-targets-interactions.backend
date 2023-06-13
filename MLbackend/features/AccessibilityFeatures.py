edee09779e88545514ff653e3283b46a7ee9b5df
uri zlotkin <urizlo@post.bgu.ac.il>
2023-06-13 17:16:04 +0300

    Finish prediction endpoint

--- MLbackend/features/AccessibilityFeatures.py
+++ MLbackend/features/AccessibilityFeatures.py
@@ -1,8 +1,13 @@
 import tempfile
 from pathlib import Path
 from subprocess import call
-
+import os
 from features.Features import Features
+import subprocess
+import requests
+import re
+
+
 
 
 class AccessibilityFeatures(Features):
@@ -10,27 +15,27 @@ class AccessibilityFeatures(Features):
         self._features_dict["accessibility"] = self.accessibility()
 
     def accessibility(self):  # 37*10 = 370
-        with tempfile.TemporaryDirectory() as tmpdirname:
-            acc_file: Path = Path(tmpdirname) / "mrna_acc.fa"
-            with open(acc_file, 'w') as f:
-                f.write(self._region_sequence.replace('-', '') + '\n')
-
-            cmd = 'RNAplfold -W 80 -L 40 -u 10 < {}'.format(acc_file)
-            status = call(cmd, cwd=Path(tmpdirname).resolve(), shell=True)
-
-            acc_outfile: Path = Path(tmpdirname) / "plfold_lunp"
-            with acc_outfile.open('r') as f:
-                ACC_allstr = f.readlines()
-
+        url = "http://localhost:3031/wsl_api/plfold?region_seq=" + self._region_sequence
+        response = requests.get(url)
+        ACC_allstr = response.text
+        ACC_allstr = ACC_allstr.split("\\n")
+        ACC_allstr = ACC_allstr[: len(ACC_allstr) - 1]
+        ACC_allstr = [s + '\\n' for s in ACC_allstr] 
+        ACC_allstr = [s.lstrip('","') for s in ACC_allstr]
+        ACC_allstr[0] = ACC_allstr[0][2:]
         acc_score_matrix = []
         for line in ACC_allstr[2:]:
             l = line.strip().replace('NA', str(0.97)).split()
             acc_score_matrix.append(l)
-
         acc_score_matrix = [[0.0] * 11] * 37 + acc_score_matrix + [[0.0] * 11] * 37
+        acc_score_matrix = [line[0].split("\\t") if isinstance(line[0], str) else line for line in acc_score_matrix]
         acc_score_matrix_segment = acc_score_matrix[self._end + 15:self._end + 52]
+        acc_score_matrix_segment = [line.split("\\t") if isinstance(line, str) else line for line in acc_score_matrix_segment]
+        for i in range(len(acc_score_matrix_segment)):
+            for j in range(len(acc_score_matrix_segment[i])):
+                if '\\n' in acc_score_matrix_segment[i][j]:
+                    acc_score_matrix_segment[i][j] = acc_score_matrix_segment[i][j].replace('\\n', "")
         ACC = {}
-
         try:
             for i in range(1, 38):
                 for j in range(1, 11):
@@ -40,5 +45,4 @@ class AccessibilityFeatures(Features):
             print('was except')
             print(self._region_sequence)
             ACC[key] = float(0.0)
-
         return ACC
